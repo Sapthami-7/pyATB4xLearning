3c7dc57aa15537df40e2eaae78e0f9caf8730ca7
sapthami-7 <shettysapthami@gmail.com>
2024-08-13 21:56:44 +0530

    lab02

---
+++ src/ex_132024/HWTable9.py
@@ -0,0 +1,37 @@
+# Table of 9
+print("enter the table of 9")
+table = 9
+print(f"{table} * 1 = "f"{table * 1}")
+print(f"{table} * 2 = "f"{table * 2}")
+print(f"{table} * 3 = "f"{table * 3}")
+print(f"{table} * 4 = "f"{table * 4}")
+print(f"{table} * 5 = "f"{table * 5}")
+print(f"{table} * 6 = "f"{table * 6}")
+print(f"{table} * 7 = "f"{table * 7}")
+print(f"{table} * 8 = "f"{table * 8}")
+print(f"{table} * 9 = "f"{table * 9}")
+print(f"{table} * 10 = "f"{table * 10}")
+
+# input the value to print the table with multiply till  10
+table1 = int(input("enter the table for any num:"))
+print(f"{table1} * 1 = "f"{table1 * 1}")
+print(f"{table1} * 2 = "f"{table1 * 2}")
+print(f"{table1} * 3 = "f"{table1 * 3}")
+print(f"{table1} * 4 = "f"{table1 * 4}")
+print(f"{table1} * 5 = "f"{table1 * 5}")
+print(f"{table1} * 6 = "f"{table1 * 6}")
+print(f"{table1} * 7 = "f"{table1 * 7}")
+print(f"{table1} * 8 = "f"{table1 * 8}")
+print(f"{table1} * 9 = "f"{table1 * 9}")
+print(f"{table1} * 10 = "f"{table1 * 10}")
+
+#Take 2 inputs from user and find max,pow,sum,dif,mult,div
+
+input1 = int(input("Enter the val1: "))
+input2 = int(input("Enter the val2: "))
+print(f"maximum val between {input1} and {input2}: ", max(input1, input2))
+print(f"power of val between {input1} and {input2}: ", pow(input1, input2))
+print(f"sum of  {input1} and {input2}:", input1 + input2)
+print(f"subtract of {input1} and {input2} : ", input1 - input2)
+print(f"multiply of {input1},{input2}: ", input1 * input2)
+print(f"division of f{input1},f{input2}:", input1 / input2)
---
+++ src/ex_132024/Lab001.py
@@ -0,0 +1,9 @@
+age = 78
+print(age)
+
+ages = 9
+
+print(ages)
+# float
+age=4.5
+print(age)
